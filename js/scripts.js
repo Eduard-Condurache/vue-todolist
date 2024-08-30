d4804bfdcb6bac135e157ffc42ea1d26949c8c69
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-08-30 15:54:16 +0200

    Completato esercizio con i bonus.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ createApp({
        todos: [
           {
             text: 'Fare la spesa',
-            done: true
+            done: false
           },
           {
             text: 'Ripassare i esercizi di vue',
@@ -20,12 +20,29 @@ createApp({
             text: 'Stilizzare pagina todoo fatta con vue anche nel weekend',
             done: false
           }
-       ]
+       ],
+       newTodo: ''
     };
   },
   methods: {
+    addTodo() {
+      //Se la variabile newTodo non è vuota aggiungi alla lista
+      //todos un nuovo oggetto e poi svuota l'input.
+      if (this.newTodo.trim() !== '') {
+        // Il metodo trim serve per rimuovere i spazi bianchi
+          this.todos.push({text: this.newTodo.trim(), done: false});
+      }
+          this.newTodo = '';
+    },
     removeTodo(i) {
-      this.todos.splice(i, 1); // Il metodo splice in questo caso rimuove un elemento dal array.
+      // Il metodo splice in questo caso rimuove un elemento dal array.
+      // quindi in questo caso la i sta per l'indice del array e il 1 specifica il numero di elementi da rimuovere.
+      this.todos.splice(i, 1);
+    },
+    invertedValue(todo) {
+      // Scambia il valore booleano di todo.done al click
+      // quindi se todo.done è false diventa true e viceversa.
+      todo.done = !todo.done;
     }
   }
 }).mount('#app')
