36ba1a2a90c2a7a644642200e3dae6a6b165c24a
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-08-30 14:39:22 +0200

    Completato milestone1

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,24 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
-    }
-  }
+       todos: [
+          {
+            text: 'Fare la spesa',
+            done: true
+          },
+          {
+            text: 'Ripassare i esercizi di vue',
+            done: false
+          },
+          {
+            text: 'Sistemare il computer di Salvatore',
+            done: false
+          },
+          {
+            text: 'Stilizzare pagina todoo fatta con vue anche nel weekend',
+            done: false
+          }
+       ]
+    };
+  },
 }).mount('#app')
